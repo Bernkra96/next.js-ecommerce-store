b3e03fc9c444b9fdff4c116fc76e1fd3d77b2d09
Mona Lisa Octocat <bernkra@gmail.com>
2023-09-30 20:47:59 +0200

    Add ccs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { headers } from 'next/headers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,21 +18,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/cart">Shopping Cart</a>
-
+        <header>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/cart">Shopping Cart</a>
+        </header>
         {children}
+        <footer>
+          <h3> Thanks For Shopping </h3>
+          <h4> Impressum</h4>
+          <p> Musster Inc.</p>
+          <p> Mustter Steasse 1 /2 </p>
+          <p> 1111 MustterStatt AT </p>
+          <p> Tel : 00 43 11 65555 </p>
 
-        <h3> Thanks For Shopping </h3>
-        <h4> Impressum</h4>
-        <p> Musster Inc.</p>
-        <p> Mustter Steasse 1 /2 </p>
-        <p> 1111 MustterStatt AT </p>
-        <p> Tel : 00 43 11 65555 </p>
+          <p>E-Mail : office@musster.inc.at </p>
 
-        <p>E-Mail : office@musster.inc.at </p>
-        <p> Webb : musster.inc.at </p>
+          <p> Web : musster.inc.at </p>
+        </footer>
       </body>
     </html>
   );
